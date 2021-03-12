31fb25fae271579f3905febf05c25980f5c70d10
Cahyo <cariercahyo@gmail.com>
2021-03-12 15:10:17 +0900

    update test

--- var_data/test.py
+++ var_data/test.py
@@ -8,9 +8,16 @@ celana = {
 	'harga':200000,
 	'diskon':230990
 }
-daftar_belanja = [sepatu, celana]
+# daftar_belanja = [sepatu, celana]
 
-totalCelana = celana['harga'] - celana['diskon']
-totalPajak = totalCelana * 0.1
-print(totalPajak)
+# totalCelana = celana['harga'] - celana['diskon']
+# totalPajak = totalCelana * 0.1
+# print(totalPajak)
 
+totalHarga = 150000
+potonganHarga = 0.3
+pajak = 0.1
+hargaBayar = (1 - 0.3) * totalHarga
+hargaBayar += hargaBayar * pajak
+
+print(5 % 3 ** 2)
