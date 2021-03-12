511d32a40581ac677db2da279292ede7cbaf3f18
Cahyo <cariercahyo@gmail.com>
2021-03-12 14:13:14 +0900

    update test

--- var_data/test.py
+++ var_data/test.py
@@ -4,5 +4,13 @@ sepatu = {
 	"diskon":300000
 }
 celana = {
-	
+	"nama":'Celana Lepis',
+	'harga':200000,
+	'diskon':230990
 }
+daftar_belanja = [sepatu, celana]
+
+totalCelana = celana['harga'] - celana['diskon']
+totalPajak = totalCelana * 0.1
+print(totalPajak)
+
