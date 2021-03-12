ca685cbc9827af6f434d3f4c86498d0f256b548e
Cahyo <cariercahyo@gmail.com>
2021-03-12 17:07:18 +0900

    add task

---
+++ task/data_warehouse.py
@@ -0,0 +1,25 @@
+user = input('masukkan nama anda')
+warehousing = {
+	'harga':1000000,
+	'total':15
+}
+cleansing = {
+	'harga':1500000,
+	'total':10
+}
+integration = {
+	'harga':2000000,
+	'total':15
+}
+transformation = {
+	'harga':2500000,
+	'total':10
+}
+sub_warehousing = warehousing['harga'] * warehousing['total']
+sub_cleansing = cleansing['harga'] * cleansing['total']
+sub_integration = integration['harga'] * integration['total']
+sub_transformation = transformation['harga'] * transformation['total']
+sub_total = sub_warehousing + sub_cleansing + sub_integration + sub_transformation
+print('Tagihan Kepada:', user)
+print("Selamat pagi, anda harus membayar tagihan sebesar:")
+print(sub_warehousing)
