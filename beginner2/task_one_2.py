f918324c47bba4a32967111c874354beba055557
Cahyo <cariercahyo@gmail.com>
2021-03-16 02:24:16 +0900

    add penggabungan dua list

--- beginner2/task_one_2.py
+++ beginner2/task_one_2.py
@@ -10,3 +10,9 @@ print(pertengahan_tahun)
 print('awal tahun', awal_tahun)
 print('akhir tahun', akhir_tahun)
 print(bulan[-4:-1])
+
+#!penggabungan dua list atau lebih
+list_makanan = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_minuman = ['Es Teh', 'Es Jeruk', 'Es Campur']
+list_menu = list_makanan + list_minuman
+print(list_menu)
