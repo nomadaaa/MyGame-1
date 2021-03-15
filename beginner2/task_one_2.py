d8f1c039f1a72cc7cb64bb5e76684d660b9badfa
Cahyo <cariercahyo@gmail.com>
2021-03-16 02:09:16 +0900

    update task

--- beginner2/task_one_2.py
+++ beginner2/task_one_2.py
@@ -1,5 +1,12 @@
 bulan = ('Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
 )
+
+#penggunaan list
 pertengahan_tahun = bulan[4:9]
+awal_tahun = bulan[:5]
+akhir_tahun = bulan[8:]
 print(pertengahan_tahun)
+print('awal tahun', awal_tahun)
+print('akhir tahun', akhir_tahun)
+print(bulan[-4:-1])
