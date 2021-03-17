96253097b53768631360eb3ceb87541b7d8c9e37
Cahyo <cariercahyo@gmail.com>
2021-03-18 00:41:24 +0900

    add sort

--- beginner2/list_manipulation2.py
+++ beginner2/list_manipulation2.py
@@ -7,12 +7,19 @@ score_pertama_sud = list_score.index('Sud')
 #! menambahkan elemen
 list_score.insert(3, 'Sud')
 
+#! method remove()
+list_menu.remove('Rendang')
+
 #! method pop
 #! pop adalah menghilangkan element berdasarkan index
 list_menu.pop(1)
 
 #! method reverse
+#! membalik urutan element
 list_menu.reverse()
+
+#! method sort
+list_menu.sort()
 
 #?print
 print(list_score)
