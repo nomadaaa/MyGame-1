d3de2852d341a97471dca324a8fb2bcae42ba33c
Cahyo <cariercahyo@gmail.com>
2021-03-18 00:36:45 +0900

    add pop

--- beginner2/list_manipulation2.py
+++ beginner2/list_manipulation2.py
@@ -1,3 +1,4 @@
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang']
 list_score = ['Budi','Sud','Budi','Budi','Budi','Sud','Sud']
 #! method index
 score_pertama_sud = list_score.index('Sud') 
@@ -5,6 +6,9 @@ score_pertama_sud = list_score.index('Sud')
 #! method insert
 list_score.insert(3, 'Sud')
 
+#! method pop
+list_menu.pop(1)
 
 #?print
 print(list_score)
+print(list_menu)
