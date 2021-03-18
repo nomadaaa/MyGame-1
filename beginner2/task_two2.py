b403515c53455fa80a5fd5004ca4f3c902e7afa8
Cahyo <cariercahyo@gmail.com>
2021-03-18 22:01:24 +0900

    update

--- beginner2/task_two2.py
+++ beginner2/task_two2.py
@@ -4,4 +4,5 @@ info_karyawan.clear()
 #!copy
 info_karyawan1 = {'nama' : 'Aksara','nik' : '1211011','pekerjaan' : 'Data Analyst'}
 info_karyawan2 = info_karyawan1.copy()
+info_karyawan2['nama'] = 'Cahyo'
 print(info_karyawan2)
