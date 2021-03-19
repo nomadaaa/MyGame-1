68c7b643182061e71230c237f58b73074543047a
Cahyo <cariercahyo@gmail.com>
2021-03-19 16:21:15 +0900

    update

---
+++ beginner2/string_manipulation.py
@@ -0,0 +1,11 @@
+komen = """Ini contoh"""
+print(komen)
+
+nama_produk = 'Sepatu Niko'
+nama_produk = nama_produk[:2] + 'P' + nama_produk[3:9] + 'K' + nama_produk[-1]
+print(nama_produk)
+
+paragrap = 'ini adalah paragrap pertama yang dibuat'
+paragrap = paragrap[:0] + 'I' + paragrap[11:] + 'P'
+print(paragrap)
+print(len(paragrap))
