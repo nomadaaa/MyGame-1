7ad37d3494b9bf38816ad1385c24423263c83bfa
Cahyo <cariercahyo@gmail.com>
2021-03-19 16:21:25 +0900

    update

--- beginner2/string_manipulation.py
+++ beginner2/string_manipulation.py
@@ -6,6 +6,6 @@ nama_produk = nama_produk[:2] + 'P' + nama_produk[3:9] + 'K' + nama_produk[-1]
 print(nama_produk)
 
 paragrap = 'ini adalah paragrap pertama yang dibuat'
-paragrap = paragrap[:0] + 'I' + paragrap[11:] + 'P'
+paragrap = paragrap[:0] + 'I' + paragrap[11:19] + 'P'
 print(paragrap)
 print(len(paragrap))
