4e3ec89fb2cffe01525f495ba80d00a6fd80a321
Cahyo <cariercahyo@gmail.com>
2021-03-20 22:04:07 +0900

    update task1

--- beginner3/task1.py
+++ beginner3/task1.py
@@ -2,11 +2,6 @@
 
 #* membuat class
 class Karyawan:
-	#* membuat variabel jam
-	#* menghitung berapa lama pegawai lembur
-	#* > 6 == 500
-	#* 250
-	jam = 0
 
 	#* func init
 	def __init__(self, nama, usia, pendapatan, insentif_lembur=0):
@@ -17,7 +12,11 @@ class Karyawan:
 
 	#* membuat fungsi lembur
 	#* kita check berapa jam pegawai lembur
-	def lembur(self):
+	def lembur(self, jam=0):
+			#* membuat variabel jam
+			#* menghitung berapa lama pegawai lembur
+			#* > 6 == 500
+			#* 250
 		self.jam = jam
 		if jam >= 6:
 			self.insentif_lembur += 500000
@@ -26,4 +25,5 @@ class Karyawan:
 
 #* execute program
 pegawai1 = Karyawan('Agum', 22, 3000000)
+pegawai1.lembur()
 print(pegawai1.nama)
