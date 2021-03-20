1a6f555336452de427debf6270a4e1e5db47fc64
Cahyo <cariercahyo@gmail.com>
2021-03-20 15:14:19 +0900

    update

--- beginner3/part1.py
+++ beginner3/part1.py
@@ -1,7 +1,9 @@
 
 #* membuat class karyawan yang akan berfungsi sebagai blueprint
-#* 
+#* class biasanya menggunakan huruf kapital pada awal hurufnya
 class Karyawan:
 	pass
 
+#* ini merupakan contoh objek dari class karyawan
 agum = Karyawan()
+senja = Karyawan()
