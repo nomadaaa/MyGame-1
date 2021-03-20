cea1405f4cf665255fcd81604a93b939432cc657
Cahyo <cariercahyo@gmail.com>
2021-03-20 15:24:57 +0900

    update

--- beginner3/part1.py
+++ beginner3/part1.py
@@ -1,9 +1,18 @@
 
 #* membuat class karyawan yang akan berfungsi sebagai blueprint
 #* class biasanya menggunakan huruf kapital pada awal hurufnya
+#* di dalam class mempunyai atribut dan properti
 class Karyawan:
-	pass
+	#* salah satu penggunaan class attribut
+	nama_perusahaan = 'ABC'
+	# pass
 
 #* ini merupakan contoh objek dari class karyawan
 agum = Karyawan()
 senja = Karyawan()
+
+
+#* mengubah isi dari attribut class
+agum.__class__.nama_perusahaan = 'DEF'
+#* mengakases attribut dari class
+print(agum.__class__.nama_perusahaan)
