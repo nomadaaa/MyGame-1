ee11008b599150216cb96d6d2f9094dcafb73b19
Cahyo <cariercahyo@gmail.com>
2021-03-20 15:42:42 +0900

    update

--- beginner3/part1.py
+++ beginner3/part1.py
@@ -5,10 +5,21 @@
 class Karyawan:
 	#* salah satu penggunaan class attribut
 	nama_perusahaan = 'ABC'
+	#* didalam sebuah class biasanya terdapat sebuah constructor
+	#* constructor di python disebut __init__()
+	#* fungsi cons yaitu kita bisa menginisialisasi attribut milik objek
+	#* contoh
+	#* self ialah parameter pertama yang dibawa oleh semua function
+	#* kemudian dilanjutkan dengan parameter2 yg lain
+	def __init__(self, nama, umur, gaji):
+		self.nama = nama
+		self.gaji = gaji
+		self.umur = umur
+
 	# pass
 
 #* ini merupakan contoh objek dari class karyawan
-agum = Karyawan()
+agum = Karyawan('Agum', 20, 10000000)
 senja = Karyawan()
 
 
